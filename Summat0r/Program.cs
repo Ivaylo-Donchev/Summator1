5a6b43bc19aa4f1428314f4b5c4a446f20318ebc
IvailitoMan <ivailitoman@gmail.com>
2023-02-08 12:43:21 +0200

    Add project files.

---
+++ Summat0r/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Summat0r
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Summator.Test_SumTwoNumbers();
+            // Console.WriteLine(" Sum = " + Summator.Sum(new int[] {1, 2, 3}));
+        }
+    }
+}
